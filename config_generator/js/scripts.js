b402cdde20d9c9eeb647a94e3ae247daf243412a
masterperson40 <masterperson40@gmail.com>
2015-07-01 18:15:17 -0700

    Added config_generator

---
+++ config_generator/js/scripts.js
@@ -0,0 +1,38 @@
+/**************************************************************/
+/* Prepares the cv to be dynamically expandable/collapsible   */
+/**************************************************************/
+function prepareList() {
+    $('#expList').find('li:has(ul)')
+    .click( function(event) {
+        if (this == event.target) {
+            $(this).toggleClass('expanded');
+            $(this).children('ul').toggle('medium');
+        }
+        return false;
+    })
+    .addClass('collapsed')
+    .children('ul').hide();
+
+    //Create the button funtionality
+    $('#expandList')
+    .unbind('click')
+    .click( function() {
+        $('.collapsed').addClass('expanded');
+        $('.collapsed').children().show('medium');
+    })
+    $('#collapseList')
+    .unbind('click')
+    .click( function() {
+        $('.collapsed').removeClass('expanded');
+        $('.collapsed').children().hide('medium');
+    })
+    
+};
+
+
+/**************************************************************/
+/* Functions to execute on loading the document               */
+/**************************************************************/
+$(document).ready( function() {
+    prepareList()
+});
