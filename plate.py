ee631081af1eb05e7d2fd7f32ff73f70b30e611b
Matt Rogge <mprogge@gmail.com>
2015-07-27 19:00:04 -0700

    Added a warning for low platerRotation values

--- plate.py
+++ plate.py
@@ -4,6 +4,9 @@ import globalVars as gv
 import shutil
 import subprocess
 import platform
+import utilityFunctions as uf
+
+source = os.path.basename(__file__)
 
 def plate():
     #Make dateString and add it to the directory string
@@ -19,6 +22,13 @@ def plate():
     platerDelta = gv.platerDelta
     platerRotation = gv.platerRotation
     
+    #Check platerRotation for low value
+    minPlaterRotation = 20
+    if platerRotation <= minPlaterRotation:
+        uf.warning("Plater can take a long time to finish with Rotation values under " + str(minPlaterRotation)+".",
+                   "Low platerRotation value. platerRotation = "+str(platerRotation),
+                   gv.level, source)
+    
     #Move Files to the Right Location
     shutil.copy2(os.path.dirname(os.path.abspath(__file__))+'/plater/plater.conf', printerDir+'STL_Files/plater.conf')
     
@@ -30,8 +40,8 @@ def plate():
 	
     if platform.system()=='Darwin':    #OSX 
         shutil.copy2(os.path.dirname(os.path.abspath(__file__))+'/plater/plater-mac', printerDir+'STL_Files/plater-mac')
-	subprocess.check_output([printerDir+'STL_Files/plater-mac','-v', '-W '+ str(platerWidth), '-H '+str(platerHeight), '-j '+str(platerPrecision), '-s '+str(platerSpacing), '-d '+str(platerDelta), '-r '+str(platerRotation), printerDir+'STL_Files/plater.conf'])
-	os.remove(printerDir+'STL_Files/plater-mac') 
+        subprocess.check_output([printerDir+'STL_Files/plater-mac','-v', '-W '+ str(platerWidth), '-H '+str(platerHeight), '-j '+str(platerPrecision), '-s '+str(platerSpacing), '-d '+str(platerDelta), '-r '+str(platerRotation), printerDir+'STL_Files/plater.conf'])
+        os.remove(printerDir+'STL_Files/plater-mac') 
 	
     if platform.system()=='Linux':
         shutil.copy2(os.path.dirname(os.path.abspath(__file__))+'/plater/plater-linux', printerDir+'STL_Files/'+platerName)
