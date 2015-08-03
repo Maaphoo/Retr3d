398c1cef536bd41a223925491955d043a26f55c1
masterperson40 <masterperson40@gmail.com>
2015-08-03 12:24:15 -0600

    Removed GUI calls

--- zRodR.py
+++ zRodR.py
@@ -5,7 +5,6 @@ from itertools import product
 
 #import FreeCAD modules
 import FreeCAD as App
-import FreeCADGui as Gui
 import Part
 import Sketcher
 import Draft
@@ -21,11 +20,11 @@ class ZRodR(object):
 		App.ActiveDocument=App.getDocument(self.name)
 		shape = App.ActiveDocument.ActiveObject.Shape
 		App.ActiveDocument=App.getDocument("PrinterAssembly")
-		Gui.ActiveDocument=Gui.getDocument("PrinterAssembly")
+		#.ActiveDocument=#.getDocument("PrinterAssembly")
 		App.ActiveDocument.addObject('Part::Feature',self.name).Shape= shape
 		
 		#Color Part
-#		Gui.ActiveDocument.getObject(self.name).ShapeColor = (gv.printedR,gv.printedG,gv.printedB,gv.printedA)
+#		#.ActiveDocument.getObject(self.name).ShapeColor = (gv.printedR,gv.printedG,gv.printedB,gv.printedA)
 		
 		#Get the feature and move it into position
 		objs = App.ActiveDocument.getObjectsByLabel(self.name)
@@ -51,13 +50,13 @@ class ZRodR(object):
 
 	def draw(self):
 		try:
-			Gui.getDocument('zRodR')
-			Gui.getDocument('zRodR').resetEdit()
+			#.getDocument('zRodR')
+			#.getDocument('zRodR').resetEdit()
 			App.getDocument('zRodR').recompute()
 			App.closeDocument("zRodR")
 			App.setActiveDocument("")
 			App.ActiveDocument=None
-			Gui.ActiveDocument=None	
+			#.ActiveDocument=None
 		except:
 			pass
 
@@ -65,14 +64,14 @@ class ZRodR(object):
 		App.newDocument("zRodR")
 		App.setActiveDocument("zRodR")
 		App.ActiveDocument=App.getDocument("zRodR")
-		Gui.ActiveDocument=Gui.getDocument("zRodR")
+		#.ActiveDocument=#.getDocument("zRodR")
 		
 		#make sketch
 	#	sketch = App.activeDocument().addObject('Sketcher::SketchObject','Sketch')
 		App.activeDocument().addObject('Sketcher::SketchObject','Sketch')
 		App.activeDocument().Sketch.Placement = App.Placement(App.Vector(0.000000,0.000000,0.000000),App.Rotation(0.000000,0.000000,0.000000,1.000000))
-		Gui.activeDocument().activeView().setCamera('#Inventor V2.1 ascii \n OrthographicCamera {\n viewportMapping ADJUST_CAMERA\n  position 87 0 0 \n  orientation 0.57735026 0.57735026 0.57735026  2.0943952 \n  nearDistance -112.887\n  farDistance 287.28699\n  aspectRatio 1\n  focalDistance 87\n  height 143.52005\n\n}')
-##		Gui.activeDocument().setEdit('Sketch')
+		#.activeDocument().activeView().setCamera('#Inventor V2.1 ascii \n OrthographicCamera {\n viewportMapping ADJUST_CAMERA\n  position 87 0 0 \n  orientation 0.57735026 0.57735026 0.57735026  2.0943952 \n  nearDistance -112.887\n  farDistance 287.28699\n  aspectRatio 1\n  focalDistance 87\n  height 143.52005\n\n}')
+##		#.activeDocument().setEdit('Sketch')
 		App.ActiveDocument.Sketch.addGeometry(Part.Circle(App.Vector(50,50,0),App.Vector(0,0,1),gv.zRodDiaR))
 		App.ActiveDocument.recompute()
 		#sketch.addConstraint(Sketcher.Constraint('Coincident',0,3,-1,1)) 
@@ -80,7 +79,7 @@ class ZRodR(object):
 		App.ActiveDocument.recompute()
 		App.ActiveDocument.Sketch.addConstraint(Sketcher.Constraint('Radius',0,gv.zRodDiaR/2)) 
 		App.ActiveDocument.recompute()
-		Gui.getDocument('zRodR').resetEdit()
+		#.getDocument('zRodR').resetEdit()
 		App.getDocument('zRodR').recompute()
 				
 		#Pad sketch
@@ -88,7 +87,7 @@ class ZRodR(object):
 		App.activeDocument().Pad.Sketch = App.activeDocument().Sketch
 		App.activeDocument().Pad.Length = 10.0
 		App.ActiveDocument.recompute()
-		Gui.activeDocument().hide("Sketch")
+		#.activeDocument().hide("Sketch")
 		App.ActiveDocument.Pad.Length = gv.zRodLength
 		App.ActiveDocument.Pad.Reversed = 0
 		App.ActiveDocument.Pad.Midplane = 0
@@ -96,7 +95,7 @@ class ZRodR(object):
 		App.ActiveDocument.Pad.Type = 0
 		App.ActiveDocument.Pad.UpToFace = None
 		App.ActiveDocument.recompute()
-		Gui.activeDocument().resetEdit()
+		#.activeDocument().resetEdit()
 		
 		#set view as axiometric
-		Gui.activeDocument().activeView().viewAxometric()
+		#.activeDocument().activeView().viewAxometric()
